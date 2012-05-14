0d6c821acc6ad3eb555117d2efbe6da20b4cbea2
Daniel H <maxifoo@gmail.com>
2012-05-14 17:33:10 +0200

    Tar argumt for att andra R och C

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
 typedef struct filter_rc_t
@@ -44,7 +45,7 @@ void init_rc_filter(struct filter_rc_t * n, const double R, const double C, cons
     }  
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
 #define FREQ 42.0
 #define OMEGA (FREQ*M_PI*2)
@@ -53,9 +54,29 @@ int main(void)
   struct filter_rc_t f;
   int c,i,a;
   char buf[100];
-  float fl;
+  float fl,r,cnd,timel;
 
-  init_rc_filter(&f,5000,4.7e-6,1.0/NUM_SAMPLES);
+  r=5000;
+  cnd=4.7e-6;
+  timel=1.0/NUM_SAMPLES;
+
+  for(i=0;i<argc;i++)
+    {
+      if(!strncmp(argv[i],"-r",2))
+	{
+	  sscanf(argv[i+1],"%f\n",&r);
+	}
+      else if(!strncmp(argv[i],"-c",2))
+	{
+	  sscanf(argv[i+1],"%f\n",&cnd);
+	}
+      else if(!strncmp(argv[i],"-t",2))
+	{
+	  sscanf(argv[i+1],"%f\n",&timel);
+	}
+    }
+
+  init_rc_filter(&f,r,cnd,timel);
 
   c=fgetc(stdin);
   i=0;
