477152d38ace6456d0f8666adadc524ef08394d9
Daniel H <maxifoo@gmail.com>
2012-05-21 18:00:17 +0200

    Shifting back at 0.0 in list, list is initiated with malloc, -limit [timelimit], and -timestamp flags added

--- main.c
+++ main.c
@@ -5,8 +5,12 @@
 #include "rc_filter.h"
 #include "square.h"
 
-#define RC_TYPE 1
-#define SQUARE_TYPE 2
+#define RC_TYPE     0x00000001
+#define SQUARE_TYPE 0x00000002
+#define TIMESTAMP   0x00000100
+
+#define NUM_VALUES 0xFFFFF
+double * values;
 
 int main(int argc, char **argv)
 {
@@ -17,9 +21,9 @@ int main(int argc, char **argv)
   struct filter_rc_t f;
   int c,i,a;
   char buf[100];
-  int type;
+  unsigned long int type;
   unsigned int iter;
-  float fl,r,cnd,timel,freq,ampl,time_i;
+  float fl,r,cnd,timel,freq,ampl,time_i,time_limit;
 
   r=5000;
   cnd=4.7e-6;
@@ -27,11 +31,21 @@ int main(int argc, char **argv)
   freq=120;
   ampl=3.3;
   iter=10;
+  time_limit=5;
+  type=0;
   type=RC_TYPE;
 
-  for(i=0;i<argc;i++)
+  for(i=0;i<argc;i++)   
     {
-      if(!strncmp(argv[i],"-i",2))
+      if(!strncmp(argv[i],"-timestamp",10))
+	{
+	  type|=TIMESTAMP;
+	}
+      else if(!strncmp(argv[i],"-limit",6))
+	{
+	  sscanf(argv[i+1],"%f\n",&time_limit);
+	}
+      else if(!strncmp(argv[i],"-i",2))
 	{
 	  sscanf(argv[i+1],"%ui\n",&iter);  
 	}
@@ -69,20 +83,26 @@ int main(int argc, char **argv)
     }
 
 
-  switch(type)
+  switch(type & 0xFF)
     {
     case RC_TYPE:
-      init_rc_filter(&f,r,cnd,timel);
+      values = malloc(sizeof(double)*NUM_VALUES);
+      init_rc_filter(&f,r,cnd,timel,&values[0],NUM_VALUES);
       c=fgetc(stdin);
       i=0;
       a=0;
-      while(c!=EOF)
+      while((c!=EOF) && (a*f.T <= time_limit))
 	{
 	  if(c=='\n')
 	    {
 	      sscanf(buf,"%f\n",&fl);
-	      /*printf("%.5f\t%.5f\t%.5f\n",a*f.T,fl,filter_rc(&f,fl));*/
-	      printf("%.5f\t%.5f\n",fl,filter_rc(&f,fl));
+	      if(type & TIMESTAMP)
+		{
+		  printf("%.5f\t%.5f\n",a*f.T,filter_rc(&f,fl));
+		}else
+		{
+		  printf("%.5f\n",filter_rc(&f,fl));
+		}
 	      a++;
 	      i=0;
 	    }
@@ -90,14 +110,26 @@ int main(int argc, char **argv)
 	  c=fgetc(stdin);
 	  i++;
 	}
+      free(values);
       break;
 
     case SQUARE_TYPE:
       time_i=0.0;
-      while(1)
+      if((type & TIMESTAMP) == TIMESTAMP)
 	{
-	  printf("%f\n",square_w(ampl, freq, iter, time_i));
-	  time_i+=timel;
+	  while(time_i <= time_limit)
+	    {
+	      printf("%.5f\t%.5f\n",time_i,square_w(ampl, freq, iter, time_i));
+	      time_i+=timel;
+	    }
+	}
+      else
+	{
+	  while(time_i <= time_limit)
+	    {
+	      printf("%.5f\n",square_w(ampl, freq, iter, time_i));
+	      time_i+=timel;
+	    }
 	}
       break;
 
--- rc_filter.c
+++ rc_filter.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "rc_filter.h"
 
 double filter_rc(struct filter_rc_t * f, const double new_sample)
@@ -6,17 +7,31 @@ double filter_rc(struct filter_rc_t * f, const double new_sample)
   unsigned long int i;
   f->last_value[f->n] = new_sample;
 
-  for(i=0;i<(f->n);i++)
+  if(f->last_value[0] == 0.0000000000000f)
     {
-      f->last_value[i] *= f->b;
-      ret+=f->last_value[i];
+      for(i=1;i<(f->n);i++)
+	{
+	  f->last_value[i] *= f->b;
+	  f->last_value[i-1] = f->last_value[i];
+	  ret+=f->last_value[i];
+	}
+      f->n--;
+      printf("<");
+    } else
+    {
+      for(i=0;i<(f->n);i++)
+	{
+	  f->last_value[i] *= f->b;
+	  ret+=f->last_value[i];
+	}
+      printf(">");
+      f->n++;
     }
-  f->n++;  
 
   return ret*f->a;
 }
 
-void init_rc_filter(struct filter_rc_t * n, const double R, const double C, const double T)
+void init_rc_filter(struct filter_rc_t * n, const double R, const double C, const double T, double * last_val, const unsigned long int size)
 {
   unsigned long int i;
   n->R = R;
@@ -25,8 +40,9 @@ void init_rc_filter(struct filter_rc_t * n, const double R, const double C, cons
   n->a = (4*C*R*T)/(4*(C*R)*(C*R)-T*T);
   n->b = (-1 + (4)/(2+T/(C*R)));
   n->n = 0;
+  n->last_value = last_val;
 
-  for(i=0;i<0xFFFF;i++)
+  for(i=0;i<size;i++)
     {
       n->last_value[i] = 0;
     }  
